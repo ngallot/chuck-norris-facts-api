9f210464f5f2c3d3206e681d25c2cb97551e9dc3
ngallot <nicolas.gallot@gmail.com>
2019-11-13 21:52:35 +0100

    implement configuration file parsing

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,4 @@
+from app.config import ConfigLoader, ChuckNorrisApiConfig
+
 __version__ = '1.0.0'
+config: ChuckNorrisApiConfig = ConfigLoader.load_config(ChuckNorrisApiConfig)
--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 
-from app import __version__
+from app import __version__, config
 
 app = FastAPI(
-    title='Chuck Norris Facts API',
-    description='A RESTful API exposing Chuck Norris facts',
-    debug=True,
+    title=config.app_config.name,
+    description=config.app_config.description,
+    debug=config.app_config.debug,
     version=__version__
 )
+
 
 @app.get('/', description='API homepage')
 def root():
